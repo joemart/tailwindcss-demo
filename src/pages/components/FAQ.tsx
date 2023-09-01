02c7c2d9b5b4ffe0cec13fa48df092441dbb8a30
Joseph Martire <joemart06@gmail.com>
2023-09-01 12:59:59 -0400

    Add FAQ details

--- src/pages/components/FAQ.tsx
+++ src/pages/components/FAQ.tsx
@@ -1,6 +1,6 @@
 export default function FAQ() {
 
-    return <section id="FAQ" className="h-screen flex flex-col gap-[10%] justify-center items-center relative z-[1]">
+    return <section id="FAQ" className="h-screen flex flex-col gap-[10%] justify-center items-center relative z-[1] max-md:h-fit">
         <h1 className=" text-[48px]">FAQ</h1>
         <ul className=" w-[50%] text-[32px]">
             <li className=" border-b border-white border-solid p-[10px] gap-[10px]"><span>Where should I build my house?</span></li>
